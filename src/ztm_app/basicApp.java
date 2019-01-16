f0da057ce78f89509aa080d01d454d2fe7d5ae05
Dawid Gruszczynski <dawidg663@wp.pl>
2019-01-16 14:51:20 +0100

    na żądanie

--- src/ztm_app/basicApp.java
+++ src/ztm_app/basicApp.java
@@ -17,6 +17,7 @@ public class basicApp extends javax.swing.JFrame {
      * Creates new form basicApp
      */
     private Connection conn;
+    Integer option = 0;
     public basicApp() {
         initComponents();
     }
@@ -114,6 +115,11 @@ public class basicApp extends javax.swing.JFrame {
 
         choiceGroup.add(stanowiskoButton);
         stanowiskoButton.setText("Stanowisko");
+        stanowiskoButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                stanowiskoButtonActionPerformed(evt);
+            }
+        });
 
         mainTable.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -202,55 +208,84 @@ public class basicApp extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void personalDataButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_personalDataButtonActionPerformed
-        // TODO add your handling code here:
+        option = 0;
+        try{
+            mainTable.setModel(new PracownicyTableModel(new Pracownicy().getAll(conn)));
+        } catch(SQLException exc){
+            JOptionPane.showMessageDialog(null,"Nie udało się połączyć z bazą danych","Error",JOptionPane.ERROR_MESSAGE);
+        }
     }//GEN-LAST:event_personalDataButtonActionPerformed
 
     private void salaryButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_salaryButtonActionPerformed
-        // TODO add your handling code here:
+        option = 1;
+        //try{
+            //mainTable.setModel(new WynagrodzeniaTableModel(new Wynagrodzenia().getAll(conn)));
+        //} catch(SQLException exc){
+            //JOptionPane.showMessageDialog(null,"Nie udało się połączyć z bazą danych","Error",JOptionPane.ERROR_MESSAGE);
+        //}
     }//GEN-LAST:event_salaryButtonActionPerformed
 
     private void searchButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_searchButtonActionPerformed
         try{
-            mainTable.setModel(new PocztyTableModel(new Poczty().getAll(conn)));
+            switch (option){
+                case 0:
+                    mainTable.setModel(new PracownicyTableModel(new Pracownicy().getAll(conn)));
+                    break;
+                case 1:
+                    //mainTable.setModel(new WynagrodzeniaTableModel(new Wynagrodzenia().getAll(conn)));
+                    break;
+                case 2:
+                    //mainTable.setModel(new StanowiskaTableModel(new Stanowiska().getAll(conn)));
+                    break;
+            }
         } catch(SQLException exc){
             JOptionPane.showMessageDialog(null,"Nie udało się połączyć z bazą danych","Error",JOptionPane.ERROR_MESSAGE);
-        }
+        }        
     }//GEN-LAST:event_searchButtonActionPerformed
+
+    private void stanowiskoButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_stanowiskoButtonActionPerformed
+        option = 2;
+        //try{
+            //mainTable.setModel(new WynagrodzeniaTableModel(new Wynagrodzenia().getAll(conn)));
+        //} catch(SQLException exc){
+            //JOptionPane.showMessageDialog(null,"Nie udało się połączyć z bazą danych","Error",JOptionPane.ERROR_MESSAGE);
+        //}
+    }//GEN-LAST:event_stanowiskoButtonActionPerformed
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(basicApp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(basicApp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(basicApp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(basicApp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new basicApp().setVisible(true);
-            }
-        });
-    }
+//    public static void main(String args[]) {
+//        /* Set the Nimbus look and feel */
+//        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+//        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+//         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+//         */
+//        try {
+//            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+//                if ("Nimbus".equals(info.getName())) {
+//                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+//                    break;
+//                }
+//            }
+//        } catch (ClassNotFoundException ex) {
+//            java.util.logging.Logger.getLogger(basicApp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        } catch (InstantiationException ex) {
+//            java.util.logging.Logger.getLogger(basicApp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        } catch (IllegalAccessException ex) {
+//            java.util.logging.Logger.getLogger(basicApp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+//            java.util.logging.Logger.getLogger(basicApp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        }
+//        //</editor-fold>
+//
+//        /* Create and display the form */
+//        java.awt.EventQueue.invokeLater(new Runnable() {
+//            public void run() {
+//                new basicApp().setVisible(true);
+//            }
+//        });
+//    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel bottomPanel;
