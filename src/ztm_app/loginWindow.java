f2d2df10aece923ae77accf518e000bd92556a30
Dawid Gruszczynski <dawidg663@wp.pl>
2019-01-23 13:45:09 +0100

    Some unknown changes

--- src/ztm_app/loginWindow.java
+++ src/ztm_app/loginWindow.java
@@ -148,9 +148,7 @@ public class loginWindow extends javax.swing.JFrame {
         
         else if (login.equals(adminLogin) && pass.equals(adminPass)) {
             JOptionPane.showMessageDialog(null,"Weee, jestes adminem!!!","Success!",JOptionPane.INFORMATION_MESSAGE);
-
         }
-        
         else {
             JOptionPane.showMessageDialog(null,"Failed to connect user: " + login + " to " + " database with url: " + DB_URL+"\nLogin or password incorrect.","Error",JOptionPane.ERROR_MESSAGE);
         }
